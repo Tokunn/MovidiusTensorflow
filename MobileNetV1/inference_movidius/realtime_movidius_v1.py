4233b27967b6cbfb1648cd6524baba4188532362
Your Name <you@example.com>
2018-10-05 07:14:01 +0000

    opencv viewer

--- MobileNetV1/inference_movidius/realtime_movidius_v1.py
+++ MobileNetV1/inference_movidius/realtime_movidius_v1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 #------ parameters --------#
-FPS = 10
+FPS = 15
 IMGSIZE = 224
 #IMGSIZE = 160
 
-SHOW_IMGS = False
+SHOW_IMGS = True
+VERBOSE = False
 #--------------------------#
 
 
@@ -39,69 +40,80 @@ if IMGSIZE == 160:
     GRAPH_FILE = '../graphs/mbnet160_05.graph'
 
 
-
-IMAGENET1000 = False
-if IMAGENET1000:
-    print("Use imagenet")
-    import cate_imagenet1000
-    categories = cate_imagenet1000.categories
-    GRAPH_FILE = './models/mobilenet/graph'
-else:
-    CATEGORIES_FILE = './categories.txt'
-    with open(CATEGORIES_FILE, 'r') as f:
-        categories = f.read().split('\n')
-    print("Graph file from", GRAPH_FILE)
+CATEGORIES_FILE = './categories.txt'
+with open(CATEGORIES_FILE, 'r') as f:
+    categories = f.read().split('\n')
+print("Graph file from", GRAPH_FILE)
 
 
 TMP_IMG_NAME = './tmpimage.png'
 
-def predict(device, graph, cap):
-    print("Start predicting ...")
 
+class MeasureTime(object):
+    def __init__(self, label, verbose):
+        self.start = time.time()
+        self.label = label
+        self.verbose = verbose
+    def stop_ms(self):
+        return (time.time() - self.start)*1000
+    def show(self):
+        if self.verbose:
+            print(self.label + "{:.3f}".format(self.stop_ms()), end=' : ')
+
+
+def prepare_img(cap):
+    tmr = MeasureTime("Prep", verbose=VERBOSE)
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (IMGSIZE,IMGSIZE))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = np.asarray(frame) * (1.0/255.0)
+    tmr.show()
+    return frame
+
+def predict(graph, frame):
+    tmr = MeasureTime("Pred", verbose=VERBOSE)
+    # Write the tensor to the input_fifo and queue an inference
+    graph.LoadTensor(frame.astype(np.float16), None)
+    output, userobj = graph.GetResult()
+    predict_resl = np.argmax(output)
+    tmr.show()
+    return predict_resl, output
+
+def show_img(img, text):
+    tmr = MeasureTime("Post", verbose=VERBOSE)
+    if SHOW_IMGS:
+        cv2.putText(img, text, (0,50), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 3, cv2.LINE_AA)
+        cv2.imshow('Frame', img)
+        cv2.waitKey(1)
+    tmr.show()
+
+
+def main(device, graph, cap):
+    print("Start predicting ...")
 
     while True:
         start = time.time()
-        ret, frame = cap.read()
-        frame = cv2.resize(frame, (IMGSIZE,IMGSIZE))
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = np.asarray(frame) * (1.0/255.0)
-        #frame = np.clip(frame, 0.0, 0.6)
-        #print(frame.max(), round(frame.min(), 3), end=' ')
-        time1 = time.time()
-        print("Prep{:.3f}".format((time1 - start)*1000), end=' : ')
 
-        # Write the tensor to the input_fifo and queue an inference
-        graph.LoadTensor(frame.astype(np.float16), None)
-        output, userobj = graph.GetResult()
-        predict = np.argmax(output)
+        img = prepare_img(cap)  # Prepare Image
 
-        time2 = time.time()
-        print("Pred{:.4f}".format((time2-time1)*1000), end=' : ')
+        result, output = predict(graph, img)    # Predict Image
 
-        text = "{} {:.3f}".format(categories[predict], output[predict])
+        text = "{} {:.3f}".format(categories[result], output[result]) 
         print(text, end=' : ')
 
-        if SHOW_IMGS:
-            cv2.putText(frame, text, (0,50), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 3, cv2.LINE_AA)
-            cv2.imshow('Frame', frame)
-            k = cv2.waitKey(1)
-            if k == 27:
-                break
+        show_img(img, text)     # Show Image
 
-        stop = time.time()
-        print("Post{:.3f}".format((stop-time2)*1000), end=' : ')
-        totl = stop-start
-        print("Totl{:.3f}".format(totl*1000), end=' : ')
-        print("FPS{:.2f}".format(1/totl), end=' : ')
+        totl = time.time() - start
+        print("Totl{:.3f} : FPS{:.2f}".format(totl*1000, 1/totl), end=' : ')
         print('')
-
-    cv2.destroyAllWindows()
-
 
     return output
 
+
+
 if __name__ == '__main__':
     devices = mvnc.EnumerateDevices()
+    print("%d devices found" % len(devices))
     device = mvnc.Device(devices[0])
     device.OpenDevice()
 
@@ -120,10 +132,11 @@ if __name__ == '__main__':
     graph = device.AllocateGraph(graph_file_buffer)
 
     try:
-        predict(device, graph, cap)
+        main(device, graph, cap)
     except KeyboardInterrupt:
         print('exit')
         graph.DeallocateGraph()
         device.CloseDevice()
         cap.release()
+        cv2.destroyAllWindows()
         sys.exit()
